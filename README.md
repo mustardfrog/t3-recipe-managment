e3b86a61a0cc93e34397e48245f2a6842f4645a7
Frog <frogdoctor@tutanota.com>
2023-10-02 12:19:11 +0800

    add image Readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Ultimate Recipe App
+# Ultimate Recipe App (Demo) using t3 stack
+
+![Home Page](images/recipe-homepage.png)
+
+--------------------------------------------
+
+![Demo Recipe List](images/recipe-list.png)
--- components/Recipe.tsx
+++ components/Recipe.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Recipe({ recipe }) {
   return (
     <div className="flex px-2 py-4">
-        <Card className={cn("w-[300px] transition-all cursor-pointer hover:bg-pink-50 border-red-200")} >
+      <Card className={cn("w-[300px] transition-all cursor-pointer hover:bg-pink-50 border-red-200")} >
         <Link href={`/recipes/${recipe.id}`}>
           <CardHeader>
             <CardTitle>{recipe.title}</CardTitle>
@@ -16,12 +16,12 @@ export default function Recipe({ recipe }) {
             <p className="text-gray-500">Description</p>
             <p>{recipe.detail}</p>
           </CardContent>
-      </Link>
-      <CardFooter className="felx-col justify-end">
-        <div >
-          <Like />
-        </div>
-      </CardFooter>
+        </Link>
+        <CardFooter className="felx-col justify-end">
+          <div >
+            <Like />
+          </div>
+        </CardFooter>
       </Card>
     </div >
   )
